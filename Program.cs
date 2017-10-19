2be00de4412f454e7757c0e122506e252b039a11
FIC <sandrounib@hotmail.com>
2017-10-19 21:20:14 -0200

    Menu de Opcao

--- Program.cs
+++ Program.cs
@@ -6,7 +6,58 @@ namespace sistema_vendas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("MENU DE OPÇÕES");
+            System.Console.WriteLine("==============");
+            string opcao ="";
+            do{
+                System.Console.WriteLine("Digite a opção: ");
+                System.Console.WriteLine("1 - Cadastrar Clente");
+                System.Console.WriteLine("2 - Cadastrar Produto");
+                System.Console.WriteLine("3 - Cadastrar Venda");
+                System.Console.WriteLine("4 - Extrato Clente");
+                System.Console.WriteLine("9 - SAIR");
+
+                //recebe opção do cliente
+                opcao = Console.ReadLine();
+                switch(opcao){
+                        case "1":
+                        CadastrarCliente();
+                            break;
+                        case "2":
+                        CadastrarProduto();
+                            break;
+                        case "3":
+                        RealizarVenda();
+                            break;
+                        case "4":
+                        ExtratoCliente();
+                            break;                                                
+                }
+                
+            }while(opcao != "9");
         }
+        //Metodo Cadastrar Cliente
+        static void CadastrarCliente(){
+            string nomeCompletoCliente;
+            string emailCliente;
+            string opcao
+
+        }
+
+        //Metodo Cadastrar Produto
+        static void CadastrarProduto(){
+
+        }
+
+        //Metodo realizar Venda
+        static void RealizarVenda(){
+
+        }
+
+        //Método ExtratoCliente
+        static void ExtratoCliente(){
+
+        }
+
     }
 }
